9c126e1a8518824c1ad9773aba1bb082777dcc32
NiR- <stratege3@gmail.com>
2014-12-19 00:05:55 +0100

    Suppression dossier _site de git.

--- _site/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-	if ($('body.cv').length == 1) {
-		$('.picto-email').bind('click', function () {
-			$(this).parent().siblings('.email').fadeToggle();
-
-			return false;
-		});
-	}
-});
